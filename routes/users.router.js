e2b85079fc827f7877dc4f07a7f9c8a3e780c91c
yoselingalvan <ygalvan@flising.com>
2023-09-17 16:33:54 -0600

    first commit

---
+++ routes/users.router.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/users', (req, res) => {
+    const {limit, offset} = req.query
+    if(limit && offset){
+        res.json(
+            {
+                limit,
+                offset
+            })
+    } else {
+        res.send("No hay parametros")
+    }
+})
+
+
+export default router;
